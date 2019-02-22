80cc3583b09eca2b417b088cce68d3d616262fb7
Yusinto Ngadiman <yusinto@gmail.com>
2019-02-22 11:41:48 -0800

    Update home.js

--- src/universal/home.js
+++ src/universal/home.js
@@ -8,7 +8,7 @@ class Home extends Component {
     const min = 1;
     const max = 100;
     const randomNumber = Math.floor(Math.random() * (max - min)) + min;
-    ldClient.track('add to cart');
+    ldClient.variation('add to cart');
     this.setState({randomNumber});
   };
 
